6f80755531c35b645450ce2abae324cb930b9923
tao.yang <tao.yang@tendcloud.com>
2018-08-21 09:30:29 +0800

    init

---
+++ springboot.war.demo/src/main/java/com/github/tyang513/controller/CustomerErrorController.java
@@ -0,0 +1,26 @@
+package com.github.tyang513.controller;
+
+import org.springframework.boot.autoconfigure.web.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+
+/**
+ * 统一的错误处理类
+ * @author tao.yang
+ * @date 2018-08-20
+ */
+public class CustomerErrorController extends AbstractErrorController {
+
+    public CustomerErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+
+    // AddFormatters 来格式化所有日期字符串
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+
+
+}
